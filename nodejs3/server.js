866930c6397417735dec5cd8f3f3e7fe264d936e
jeevankumard24a <jeevankumard24a@gmail.com>
2025-07-12 14:02:58 +0530

    Added Server.js

---
+++ nodejs3/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+import cors from  'cors'
+
+const app = express();
+const PORT = 5000;
+
+// Create a connection pool (recommended for production apps)
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'mysql',
+    database: 'mysql',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+app.use(cors())
+app.use(express.json());
+
+
+
+// Sample route to fetch data
+app.post('/bankcustomer', async (req, res) => {
+    try {
+        const {customer_id,customer_name,bank_name,location}= req.body;
+
+
+
+
+        const [rows] = await pool.query('insert into customer(customer_id,customer_name,bank_name,location) values(?,?,?,?)',[customer_id,customer_name,bank_name,location]); // change table name if needed
+        res.json(rows);
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        res.status(500).json({ error: 'Database error' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
